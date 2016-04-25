3ca09d022c5e34817533d3dca9e7af2a9d99a6c2
Nick Brown <nicholas.g.brown@gmail.com>
2016-04-25 10:23:10 -0400

    Add code to new.html.erb and users_controller.rb to ceate a new user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,15 @@ class UsersController < ApplicationController
   end
 
   def new
-
+    @user = User.new
   end
 
   def show
   end
 
   def create
+    @user = User.create!(user_params)
+    redirect_to user_path
   end
 
   def edit
@@ -20,5 +22,10 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :city, :state, :country, :picture_url)
   end
 end
