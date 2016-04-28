e941d248f4a877bf386ed20e3cd40947ab4ba55d
Nick Brown <nicholas.g.brown@gmail.com>
2016-04-28 14:16:39 -0400

    I added first and last name to the user sign up process in devise. I also added the bootstrap drop down menu.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'static_pages#home'
   match '/about', to: 'static_pages#about', via: 'get'
 
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
 
   resources :users do
     resources :wines
